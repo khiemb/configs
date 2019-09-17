1bfd336eac7859ba9b4fe2078f6306bb367245b6
Khiem Bui <kb.legacy@gmail.com>
2019-09-16 23:54:25 -0500

    Added fish config file.

---
+++ config.fish
@@ -0,0 +1,10 @@
+function fish_right_prompt
+#intentionally left blank
+end
+
+status --is-interactive; and source (rbenv init -|psub)
+
+# Conda Setup
+source /home/CSCScience.ca/kbui/miniconda3/etc/fish/conf.d/conda.fish
+
+set -g theme_display_ruby no
